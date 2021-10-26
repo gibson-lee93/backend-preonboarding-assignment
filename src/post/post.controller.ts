81d3bf08e5141a875a21d1d138e6f6cbef0c950e
Gibson Lee <gb.lee.gibson@gmail.com>
2021-10-26 23:26:16 +0900

    Added getPostById function

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Post } from './post.entity';
+import { PostService } from './post.service';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+  constructor(private postService: PostService) {}
+
+  @Get('/:id')
+  getPostById(@Param('id') id: string): Promise<Post> {
+    return this.postService.getPostById(id);
+  }
+}
