bf562219c9f79c23ab5c7446bebcb92a4f666a1c
Gibson Lee <gb.lee.gibson@gmail.com>
2021-10-26 22:54:37 +0900

    Added signIn function

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,23 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersRepository } from './users.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersRepository])],
-  providers: [UserService],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'randomsecret',
+      signOptions: {
+        expiresIn: '3h',
+      },
+    }),
+    TypeOrmModule.forFeature([UsersRepository]),
+  ],
+  providers: [UserService, JwtStrategy],
   controllers: [UserController],
+  exports: [JwtStrategy, PassportModule],
 })
 export class UserModule {}
