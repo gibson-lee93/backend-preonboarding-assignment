c1245820db77557d1788eb6f7569feaffc8b7c42
Gibson Lee <gb.lee.gibson@gmail.com>
2021-10-26 20:45:45 +0900

    Created user entity and repository

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private usersRepository: UsersRepository,
+  ) {}
+}
