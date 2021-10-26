801032e71a3c0c7483d5a8775806140fded7c253
Gibson Lee <gb.lee.gibson@gmail.com>
2021-10-26 20:25:20 +0900

    Setting for sqlite connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db',
+      entities: [__dirname + '/**/*.entity.ts'],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
