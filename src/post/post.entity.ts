35e732a297f47838b12634447ef6663fd9f88a07
Gibson Lee <gb.lee.gibson@gmail.com>
2021-10-27 12:30:14 +0900

    Added relation between user and post entity

--- src/post/post.entity.ts
+++ src/post/post.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from '../user/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Post {
@@ -16,4 +17,7 @@ export class Post {
 
   @Column({ nullable: true })
   updated_at: Date;
+
+  @ManyToOne((_type) => User, (user) => user.posts, { eager: false })
+  user: User;
 }
